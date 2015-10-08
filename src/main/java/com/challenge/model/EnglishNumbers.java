15e923a8884bcd966ae873903c7b4227a1408e4f
irina <irina.i.placinta@gmail.com>
2015-10-08 22:49:46 +0100

    working version

---
+++ src/main/java/com/challenge/model/EnglishNumbers.java
@@ -0,0 +1,44 @@
+package com.challenge.model;
+
+
+/**
+ * @author iplacinta
+ *
+ */
+public class EnglishNumbers
+{
+    public static final String ZERO = "zero";
+    public static final String ONE = "one";
+    public static final String TWO = "two";
+    public static final String THREE = "three";
+    public static final String FOUR = "four";
+    public static final String FIVE = "five";
+    public static final String SIX = "six";
+    public static final String SEVEN = "seven";
+    public static final String EIGHT = "eight";
+    public static final String NINE = "nine";
+    
+    public static final String TEN = "ten";
+    public static final String ELEVEN = "eleven";
+    public static final String TWELVE = "twelve";
+    public static final String THIRTEEN = "thirteen";
+    public static final String FOURTEEN = "fourteen";
+    public static final String FIFTEEN = "fifteen";
+    public static final String SIXTEEN = "sixteen";
+    public static final String SEVENTEEN = "seventeen";
+    public static final String EIGHTEEN = "eighteen";
+    public static final String NINETEEN = "nineteen";
+    
+    public static final String TWENTY = "twenty";
+    public static final String THIRTY = "thirty";
+    public static final String FORTY = "forty";
+    public static final String FIFTY = "fifty";
+    public static final String SIXTY = "sixty";
+    public static final String SEVENTY = "seventy";
+    public static final String EIGHTY = "eighty";
+    public static final String NINETY = "ninety";
+    
+    public static final String HUNDRED = "hundred";
+    public static final String THOUSAND = "thousand";
+    public static final String MILLION = "million";
+}
---
+++ src/main/java/com/challenge/model/IntNumberFormat.java
@@ -0,0 +1,47 @@
+package com.challenge.model;
+
+/**
+ * @author iplacinta
+ *
+ */
+public class IntNumberFormat
+{
+    private int number;
+    private int div;
+    private int mod;
+    private int unit;
+    
+    public int getDiv()
+    {
+        return div;
+    }
+    public void setDiv(int div)
+    {
+        this.div = div;
+    }
+    public int getMod()
+    {
+        return mod;
+    }
+    public void setMod(int mod)
+    {
+        this.mod = mod;
+    }
+    public int getNumber()
+    {
+        return number;
+    }
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+    public int getUnit()
+    {
+        return unit;
+    }
+    public void setUnit(int unit)
+    {
+        this.unit = unit;
+    }
+    
+}
